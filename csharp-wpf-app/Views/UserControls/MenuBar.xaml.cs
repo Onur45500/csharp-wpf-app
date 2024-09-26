119a65a0a48bfaac4e2bc0eaab0858d3c6863501
Onur45500 <onur.akmese@viacesi.fr>
2024-09-26 11:15:14 +0200

    Created a menu bar with an usercontrol

--- csharp-wpf-app/Views/UserControls/MenuBar.xaml.cs
+++ csharp-wpf-app/Views/UserControls/MenuBar.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,20 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace csharp_wpf_app
+namespace csharp_wpf_app.Views.UserControls
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logique d'interaction pour MenuBar.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MenuBar : UserControl
     {
-        int counter = 0;
-
-        public MainWindow()
+        public MenuBar()
         {
             InitializeComponent();
-
-           
         }
     }
 }
