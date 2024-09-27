1b6ef28a85783e9a32a1450d9a246a4eea0f3c77
Onur45500 <onur.akmese@viacesi.fr>
2024-09-27 14:04:14 +0200

    Created an user control of an example of how to customize a window

--- csharp-wpf-app/Views/UserControls/CustomWindow.xaml.cs
+++ csharp-wpf-app/Views/UserControls/CustomWindow.xaml.cs
@@ -1,17 +1,26 @@
-﻿using csharp_wpf_app.Views.UserControls;
-using System.Collections;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
-namespace csharp_wpf_app
+namespace csharp_wpf_app.Views.UserControls
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logique d'interaction pour CustomWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class CustomWindow : UserControl
     {
-        public MainWindow()
+        public CustomWindow()
         {
             InitializeComponent();
         }
